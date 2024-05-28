2e9fb34f4312fa15657595fad34dd99ec2ec030c
thebigbeanest <tannergilliambusiness@gmail.com>
2024-05-28 12:09:08 -0400

    Filled out recipe.js and defined Schema for it.

---
+++ models/recipe.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const RecipeSchema = new Schema({
+    ingredient: { type: String, required: true },
+    cuisine: { type: Schema.Types.ObjectId, ref: 'Cuisine', required: true }
+});
+
+module.exports = mongoose.model('Recipe', RecipeSchema);
